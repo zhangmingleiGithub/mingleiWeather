edb68795cab0625c76d7a191794fae549eda3db9
zhangminglei <751875352@qq.com>
2018-07-18 23:29:52 +0800

    add db

---
+++ app/src/main/java/android/com/mingleiweather/db/Province.java
@@ -0,0 +1,37 @@
+package android.com.mingleiweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2018/7/18.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
